03de4ebdeb6016d37f71b5eba5570ba1aef4c245
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-10 19:19:52 +0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Weather from "./app_component/weather_component";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'weather-icons/css/weather-icons.css';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
+      <Weather />
     
     </div>
   );
---
+++ src/app_component/weather_component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Weather=()=> {
+    return (
+
+        <div>
+            <div className="container">
+                <div className="cards">
+                    <h1>Weather in Termiz</h1>
+                    <h5 className="py-4">
+                        <i className="wi wi-day-sunny display-1"></i>
+                    </h5>
+                    <h1 className="py-2">36&deg;</h1>
+
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+
+export default Weather;
